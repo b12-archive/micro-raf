4fdbb5191829143798d9430ddacd1b89735eb417
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-07-14 19:17:58 +0200

    Specify

--- test.js
+++ test.js
@@ -1,1 +1,23 @@
 var test = require('tape-catch');
+var raf = require('./index');
+
+test('Works', function (is) {
+  is.plan(3);
+
+  var executed = false;
+
+  raf(function () {
+    executed = true;
+    is.pass('executes the callback');
+  });
+
+  is.notOk(executed,
+    'doesn’t execute the callback synchronously'
+  );
+
+  setTimeout(function () {
+    is.ok(executed,
+      'executes the callback after ca 1/60 s'
+    );
+  }, 1000/60 + 50);
+});
